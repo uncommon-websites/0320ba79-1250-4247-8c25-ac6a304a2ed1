c56ed0b7e56e64f494a9967123d72a36b2d8b3ec
Uncle Common <krishnerkar@gmail.com>
2025-05-23 22:08:16 +0000

    Generated website updates for version: 620

--- src/lib/content/index.ts
+++ src/lib/content/index.ts
@@ -1,5 +1,5 @@
 export const CONFIG = {
-	companyName: "Uncommon",
+	companyName: "rUncommon",
 	description: "An AI Designer for startups that builds websites, decks, ads and more. Websites, decks and everything founders need for product market fit — all in one company building tool. Designed to sell, hire, raise.",
 	image: "/og.png",
 	url: "https://uncommon.app",
